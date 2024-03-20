13f4934d01953b04c448da8092099feba63b2daf
kristechhub <kmj.algario@gmail.com>
2024-03-20 17:13:41 +0800

    creating vue to do app

---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>To do lists</h1>
+        <table>
+           <thead>
+                <tr>
+                    <th>Tasks</th>
+                    <th>Action</th>
+                </tr>
+           </thead>
+           <tbody>
+            
+           </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            
+        },
+        data() {
+            
+        }
+    }
+</script>
+
+<style scoped>
+
+table, th {
+    border: 1px solid black;
+}
+
+
+</style>
